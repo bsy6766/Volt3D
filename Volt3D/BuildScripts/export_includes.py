9c9fd1144551a113192144968f4553fde87f142f
Seung Youp Baek <bsy6766@gmail.com>
2019-06-03 10:18:29 +0900

    Added more build script

---
+++ Volt3D/BuildScripts/export_includes.py
@@ -0,0 +1,23 @@
+import os, shutil
+
+# export includes
+
+src = 'Volt3D/'
+dst = 'bin/include/Volt3D/'
+
+if(os.path.exists(dst + '/Volt3D.h')):
+	os.remove(dst + '/Volt3D.h')
+shutil.copy('Volt3D.h', dst)
+
+for src_dir, dirs, files in os.walk(src):
+	dst_dir = src_dir.replace(src, dst, 1)
+	if not os.path.exists(dst_dir):
+		os.makedirs(dst_dir)
+	for file_ in files:
+		ext = os.path.splitext(file_)[1]
+		if ext == ".h":
+			src_file = os.path.join(src_dir, file_)
+			dst_file = os.path.join(dst_dir, file_)
+			if os.path.exists(dst_file):
+				os.remove(dst_file)
+			shutil.copy(src_file, dst_dir)
