c8d8316fa77a33310c4469ec3aacfe7f0b37e9c2
Seung Youp Baek <bsy6766@gmail.com>
2019-07-30 16:57:03 +0900

    Reworking on texture class...

---
+++ Volt3D/Volt3D/Texture/Texture.h
@@ -0,0 +1,45 @@
+﻿/**
+*	@file Texture.h
+*
+*	@author Seung Youp Baek
+*	@copyright Copyright (c) 2019 Seung Youp Baek
+*/
+
+#ifndef V3D_TEXTURE_H
+#define V3D_TEXTURE_H
+
+namespace v3d
+{
+	class Texture
+	{
+	protected:
+		Texture();
+
+		char* path;
+		unsigned int width;
+		unsigned int height;
+		unsigned int channels;
+		unsigned int mipLevel;
+		unsigned int layerCount;
+
+	public:
+		virtual ~Texture();
+
+		const char* getPath() const { return path; }
+		unsigned int getWidth() const { return width; }
+		unsigned int getHeight() const { return height; }
+		unsigned int getChannels() const { return channels; }
+		unsigned int getMipLevel() const { return mipLevel; }
+		unsigned int getLayerCount() const { return layerCount; }
+	};
+
+	class Texture2D : public v3d::Texture
+	{
+	private:
+		Texture2D();
+	public:
+		~Texture2D();
+	};
+}
+
+#endif
