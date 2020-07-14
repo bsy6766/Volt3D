e37fd46c113c917b55c36a595fe3a2e3361bbc6b
Seung Youp Baek <bsy6766@gmail.com>
2020-07-15 01:23:41 +0900

    Getting ready to start error system...but how...

---
+++ Volt3D/Volt3D/Error/Error.h
@@ -0,0 +1,19 @@
+/**
+*	@file Error.h
+*
+*	@author Seung Youp Baek
+*	@copyright Copyright (c) 2020 Seung Youp Baek
+*/
+
+#ifndef V3D_ERROR_H
+#define V3D_ERROR_H
+
+namespace v3d
+{
+	namespace Error
+	{
+
+	}
+}
+
+#endif
